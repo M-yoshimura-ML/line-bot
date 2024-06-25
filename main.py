9b8a67dda397ab39a0b2b3fad2780227180c4675
M-yoshimura-ML <masadrst@gmail.com>
2024-06-25 14:53:47 +0800

    fixing load debug file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, request
 
 app = Flask(__name__)
-
-DEBUG_FILE = './debug.txt'
+load_dotenv()
 
 
 @app.route('/test', methods=['GET'])
@@ -14,7 +16,7 @@ def test():
 @app.route('/', methods=['POST'])
 def hear():
     input_data = request.data
-    with open(DEBUG_FILE, 'wb') as f:
+    with open(os.environ.get('DEBUG_FILE'), 'wb') as f:
         f.write(input_data)
     return 'Data received', 200
 
