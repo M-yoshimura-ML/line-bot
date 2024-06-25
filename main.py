6d1ae81b874eb228a3ee12f63714b8ab449979b5
M-yoshimura-ML <masadrst@gmail.com>
2024-06-25 18:49:24 +0800

    fixing path to absolute

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 load_dotenv()
 
 
+DEBUG_FILE = os.environ.get('DEBUG_FILE', '/tmp/debug.txt')
+
+
 @app.route('/test', methods=['GET'])
 def test():
     data = {"message": "this is test route"}
@@ -16,7 +19,13 @@ def test():
 @app.route('/', methods=['POST'])
 def hear():
     input_data = request.data
-    with open(os.environ.get('DEBUG_FILE'), 'wb') as f:
+    print(input_data)
+    print(f"DEBUG_FILE path: {DEBUG_FILE}")
+
+    # make sure the directory and create if necessary
+    os.makedirs(os.path.dirname(DEBUG_FILE), exist_ok=True)
+
+    with open(DEBUG_FILE, 'wb') as f:
         f.write(input_data)
     return 'Data received', 200
 
